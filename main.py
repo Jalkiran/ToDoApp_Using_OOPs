b9bc3d20046b003bcc5d44c86802907905737570
Jalkiran <jalkirankaur36@gmail.com>
2026-06-14 19:06:51 +0530

    Added functionality to menu

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from lists import ToDoList
+tdl=ToDoList()
 while True:
     print('---Menu---')
     print('1.Add Task')
     print('2.View Tasks')
-    print('3.Search Task')
-    print('4.Mark Status')
-    print('5.Delete Task')
-    print('6.Exit\n')
+    print('3.Mark Status')
+    print('4.Delete Task')
+    print('5.Exit\n')
+
+    choice=int(input('Enter Your Choice:'))
+    if choice==1:
+        tdl.add_task()
+    elif choice==2:
+        tdl.show_tasks()
+    elif choice==3:
+        tdl.status()
+    elif choice==4:
+        tdl.delete_task()
+    elif choice==5:
+        print('Goodbye!')
+        break
+    else:
+        print('\n----Invalid Input----\n')
