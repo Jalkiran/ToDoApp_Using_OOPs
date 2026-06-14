055736e15f0e41907150c8d69b4324fc5c41ecc8
Jalkiran <jalkirankaur36@gmail.com>
2026-06-14 19:02:48 +0530

    Created Menu

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from lists import ToDoList
-tdl=ToDoList()
-tdl.add_task()
-tdl.show_tasks()
-tdl.status()
-tdl.delete_task()
+while True:
+    print('---Menu---')
+    print('1.Add Task')
+    print('2.View Tasks')
+    print('3.Search Task')
+    print('4.Mark Status')
+    print('5.Delete Task')
+    print('6.Exit\n')
